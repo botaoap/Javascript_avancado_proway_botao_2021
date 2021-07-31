4735f780c146b09695e6b363d6ca2fe6a9728f45
lordefps <gabrielbotaoap@gmail.com>
2021-07-31 17:15:40 -0300

    Aula 2 tratativas de formulario e page

--- aula2/nodejs/index.js
+++ aula2/nodejs/index.js
@@ -1,4 +1,5 @@
 const msg = "Olá pessoas";
+const { response } = require('express');
 // TODO: pegando os modulos do express
 var express = require('express');
 // TODO: trazendo os modulos do express para a var app
@@ -23,4 +24,27 @@ app.get('/', function(request, response) {
 app.get('/pagina2', function(request, response) {
     
     response.render("../views/pagina2.ejs")
+});
+
+app.get('/formulario', function(request, response) {
+    
+    response.render("../views/formulario.ejs")
+});
+
+app.get('/form/:nome', function(request, response) {
+    let varNome = request.params.nome;
+    if (varNome == 'teste') {
+        response.render("../views/formulario.ejs")
+    } else {
+        response.send("Nome: " + varNome)
+    }
+});
+
+app.get('/form2/:nome?', function(request, response) {
+    let varNome = request.params.nome;
+    if (varNome == 'teste') {
+        response.send("Nome: " + varNome)
+    } else {
+        response.render("../views/formulario.ejs")
+    }
 });
