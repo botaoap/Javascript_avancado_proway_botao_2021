3f25b5ce574a3dc71fae23daca33bad4bde85516
lordefps <gabrielbotaoap@gmail.com>
2021-07-31 14:53:50 -0300

    Aula 2 Criando um servidor com express

--- aula2/nodejs/index.js
+++ aula2/nodejs/index.js
@@ -1,3 +1,29 @@
-const msg = "Olá pessoas"
+const msg = "Olá pessoas";
+// TODO: pegando os modulos do express
+var express = require('express');
+// TODO: trazendo os modulos do express para a var app
+var app = express();
 
-console.log(msg)
+// TODO: usando um modulo do express chamando Set(olhar documentation) 
+app.set('view engine', 'ejs');
+
+// TODO: criando um Servidor na porta 3000 para rodar a aplicacao
+app.listen(3000, function() {
+    // TODO: fazendo um console apenas para identificar que o servidor esta subindo
+    console.log('Executando em http://localhost:3000')
+});
+
+// TODO: criando primeira pagina
+app.get('/', function(request, response) {
+    let texto = '<h1> Titulo da pagina </h1> <p>'+ msg +'</p>' 
+    texto += '<p><a id="botao" href="http://localhost:3000/index"> Próxima Pagina </a></p>'
+    // TODO: resposta para o servidor
+    response.send(texto)
+});
+
+// TODO: Criando uma segunda pagina
+app.get('/index', function(request, response) {
+    let texto = '<h1> Pagina Index </h1> <p> Criando outra pagina </p>' 
+    texto += '<p><a href="http://localhost:3000"> Voltar </a></p>'
+    response.send(texto)
+});
