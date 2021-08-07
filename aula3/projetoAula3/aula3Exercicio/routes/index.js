6a8705dceae2756338052fc8802cfa7e6c14d621
lordefps <gabrielbotaoap@gmail.com>
2021-08-07 18:20:07 -0300

    Navegando pelo site

--- aula3/projetoAula3/aula3Exercicio/routes/index.js
+++ aula3/projetoAula3/aula3Exercicio/routes/index.js
@@ -3,7 +3,19 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Exercicio aula 3' });
+});
+
+router.get('/formulario', function(req, res, next) {
+  res.render('formulario', { title: 'Formulario' });
+});
+
+router.get('/calculadora', function(req, res, next) {
+  res.render('calculadora', { title: 'Calculadora' });
+});
+
+router.get('/paginaRandom', function(req, res, next) {
+  res.render('paginaRandom', { title: 'Pagina Random' });
 });
 
 module.exports = router;
